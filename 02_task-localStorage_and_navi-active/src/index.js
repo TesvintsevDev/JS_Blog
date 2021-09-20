e2273f3bae6403cdeb12320d1236aeceefb725fe
TesvintsevDev <tesvintsev@gmail.com>
2021-09-20 15:49:03 +0300

    localStorage and navi-active

---
+++ 02_task-localStorage_and_navi-active/src/index.js
@@ -0,0 +1,6 @@
+import {HeaderComponent} from './components/header.component'
+import {NavigationComponent} from './components/navigation.component'
+
+new HeaderComponent('header')
+
+const navigation = new NavigationComponent('navigation')
