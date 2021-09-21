fac2051c7d51700ecc91c096638cbd7f6cf787fb
TesvintsevDev <tesvintsev@gmail.com>
2021-09-21 03:01:42 +0300

    tabs and form-validators

---
+++ 03_task-tabs_and_forms_validations/src/components/create.component.js
@@ -0,0 +1,33 @@
+import { Component } from '../core/component'
+import { Form } from '../core/form'
+import { Validators } from '../core/validators'
+
+export class CreateComponent extends Component {
+  constructor(id) {
+    super(id)
+  }
+
+  init() {
+    this.$el.addEventListener('submit', submitHandler.bind(this))
+
+    this.form = new Form(this.$el, {
+      title: [Validators.required],
+      fulltext: [Validators.required, Validators.minLength(10)]
+    })
+  }
+}
+
+function submitHandler(event) {
+  event.preventDefault()
+
+  if (this.form.isValid()) {
+    const formData = {
+      type: this.$el.type.value,
+      ...this.form.value()
+    }
+  
+    console.log('Submit', formData)
+  } else {
+    console.warn('Form is invalid')
+  }
+}
---
+++ 03_task-tabs_and_forms_validations/src/core/form.js
@@ -0,0 +1,31 @@
+export class Form {
+  constructor(form, controls) {
+    this.form = form
+    this.controls = controls
+  }
+
+  value() {
+    const value = {}
+    Object.keys(this.controls).forEach(control => {
+      value[control] = this.form[control].value
+    })
+    return value
+  }
+
+  isValid() {
+    let isFormValid = true
+
+    Object.keys(this.controls).forEach(control => {
+      const validators = this.controls[control]
+
+      let isValid = true
+      validators.forEach(validator => {
+        isValid = validator(this.form[control].value) && isValid
+      })
+
+      isFormValid = isFormValid && isValid
+    })
+
+    return isFormValid
+  }
+}
