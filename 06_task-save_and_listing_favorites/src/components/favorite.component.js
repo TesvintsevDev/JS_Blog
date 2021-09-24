c914659e36cdc8063bcd17c21d525dc3b737139f
TesvintsevDev <tesvintsev@gmail.com>
2021-09-24 18:00:36 +0300

    save and listing favorites

---
+++ 06_task-save_and_listing_favorites/src/components/favorite.component.js
@@ -0,0 +1,55 @@
+import { Component } from '../core/component'
+import { apiService } from '../services/api.service'
+import { renderPost } from '../templates/post.template'
+
+export class FavoriteComponent extends Component {
+  constructor(id, options) {
+    super(id)
+
+    this.loader = options.loader
+  }
+
+  init() {
+    this.$el.addEventListener('click', linkClickHandler.bind(this))
+  }
+
+  onShow() {
+    const favorites = JSON.parse(localStorage.getItem('favorites'))
+    const html = renderList(favorites)
+    this.$el.insertAdjacentHTML('afterbegin', html)
+  }
+
+  onHide() {
+    this.$el.innerHTML = ''
+  }
+}
+
+async function linkClickHandler(event) {
+  event.preventDefault()
+
+  if (event.target.classList.contains('js-link')) {
+    const postId = event.target.textContent
+    this.$el.innerHTML = ''
+    this.loader.show()
+    const post = await apiService.fetchPostsById(postId)
+    this.loader.hide()
+    this.$el.insertAdjacentHTML(
+      'afterbegin',
+      renderPost(post, { withButton: false })
+    )
+  }
+}
+
+function renderList(list = []) {
+  if (list.length) {
+    return `
+     <ul>
+       ${list
+         .map((i) => `<li><a href="#" class="js-link">${i}</a></li>`)
+         .join(' ')}
+     </ul>
+    `
+  }
+
+  return `<p class="center">Вы пока ничего не добавили</p>`
+}
